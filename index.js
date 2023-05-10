3a85f576081db3de4955c47fb007b236650932f7
Danik Karagodin <51264803+DanikKaragodin@users.noreply.github.com>
2023-05-10 12:40:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 const port = process.env.PORT || 5000;
 mongoose
-.connect("mongodb+srv://karagodindanik:123@cluster0.yuta3v9.mongodb.net/Drivers?retryWrites=true&w=majority")
+.connect(process.env.MONGODB_URI)
 .then(() =>{
     console.log("DB OK");
 })
